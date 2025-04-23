54e21e9ca441ed1c5586f473be3c57cdb8276d37
Kelsit0 <ramosreynagaoscar6@gmail.com>
2025-04-22 19:12:41 -0600

    login a medias, y navegacion a home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
 
@@ -7,6 +8,10 @@ export const routes: Routes = [
     {
         path:'',
         component:LoginComponent
-    }
-
+    },
+    {
+        path:'home',
+        component:HomeComponent
+    },
+    
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- Añade esta línea
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
+///aca aplica binding
 export class LoginComponent {
 
+  ///unidireccional binding 
+  email:string ='';
+  password:string ='';
+
+  constructor(private router: Router){
+    
+  }
+  submit():void{
+    console.log("credenciales", this.email, this.password)
+    this.router.navigate(['/home'])
+  }
 }
